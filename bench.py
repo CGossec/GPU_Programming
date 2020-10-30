e6ad9bcf24122613cfab604d4e4405528b78ef00
Celian GOSSEC <celian.gossec@epita.fr>
2020-10-30 16:14:13 +0100

    bench: Now with plotting, only missing histograms

--- bench.py
+++ bench.py
@@ -3,7 +3,7 @@ import argparse
 import os
 import re
 import pandas as pd
-import matplotlib.pyplot as p
+import matplotlib.pyplot as plt
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -29,12 +29,12 @@ data = ["./datatest/small_30"]
 
 modes = ["CPU", "GPU"]
 performances = {"CPU":[], "GPU":[], "nbPoints":[], "nbIterations":[-1]}
-for test in data:
+for ii, test in enumerate(data):
     source = test + "_src"
     target = test + "_tgt"
     with open(source, "r") as f:
         performances["nbPoints"].append(len(f.readlines()) - 1)
-    for mode in modes:
+    for jj, mode in enumerate(modes):
         if args.make:
             os.system("cd {0}; make; cd ..;".format(mode))
         exec_cmd = "./" + mode + "/icp ./datatest/" + source + " ./datatest/" + target
@@ -51,8 +51,15 @@ for test in data:
                 seconds = int(actual_time.split("m")[0]) * 60 + int(actual_time.split("m")[1].split(",")[0]) + float(actual_time.split("m")[1].split(",")[1][:-1]) / 1000
                 wall_times.append(seconds)
         mean = round(sum(wall_times) / len(wall_times), 5)
-        print("Average execution time over {0} iterations was {1} seconds for icp({2}_src, {2}_tgt) on {3}".format(args.iterations, mean, test, mode))
         performances[mode].append(mean)
         os.system("rm time.txt icp.stderr")
+    print("{0}% of ICPs completed. Please wait a little longer.".format(round((ii + 1) / len(data) * 100), 1))
 df = pd.DataFrame(data=performances)
 print(df)
+fig, ax = plt.subplots(1, 1, figsize=(15,9))
+ax.set_xlabel("Number of points")
+ax.set_ylabel("Speed in seconds")
+ax.plot(df.nbPoints, df.CPU, "o", label="CPU implementation")
+ax.plot(df.nbPoints, df.GPU, "o", label="GPU implementation")
+ax.legend()
+fig.savefig("benchmarks.png")
