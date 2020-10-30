7606c2535ca09a7b92e880aa1e8cac35be795fa8
Celian GOSSEC <celian.gossec@epita.fr>
2020-10-30 16:29:52 +0100

    bench: fixed parsing issues, better prints

--- bench.py
+++ bench.py
@@ -28,12 +28,13 @@ data = ["./datatest/small_30"]
 
 
 modes = ["CPU", "GPU"]
-performances = {"CPU":[], "GPU":[], "nbPoints":[], "nbIterations":[-1]}
+performances = {"CPU":[], "GPU":[], "nbPoints":[], "nbIterations":[]}
 for ii, test in enumerate(data):
     source = test + "_src"
     target = test + "_tgt"
     with open(source, "r") as f:
         performances["nbPoints"].append(len(f.readlines()) - 1)
+    performances["nbIterations"].append(-1)
     for jj, mode in enumerate(modes):
         if args.make:
             os.system("cd {0}; make; cd ..;".format(mode))
@@ -48,12 +49,14 @@ for ii, test in enumerate(data):
         for elm in lines:
             if elm.startswith("real"):
                 actual_time = elm.split()[-1]
-                seconds = int(actual_time.split("m")[0]) * 60 + int(actual_time.split("m")[1].split(",")[0]) + float(actual_time.split("m")[1].split(",")[1][:-1]) / 1000
+                minutes_to_seconds = int(actual_time.split("m")[0]) * 60
+                parse_seconds = re.split(",| \.", actual_time.split("m")[1])
+                seconds = minutes_to_seconds + int(parse_seconds[0]) + float(parse_seconds[1][:-1]) / 1000
                 wall_times.append(seconds)
         mean = round(sum(wall_times) / len(wall_times), 5)
         performances[mode].append(mean)
         os.system("rm time.txt icp.stderr")
-    print("{0}% of ICPs completed. Please wait a little longer.".format(round((ii + 1) / len(data) * 100), 1))
+        print("{0}% of ICPs completed. Please wait a little longer.".format(round((ii * 2 + jj + 1) / (len(data) * 2) * 100), 2))
 df = pd.DataFrame(data=performances)
 print(df)
 fig, ax = plt.subplots(1, 1, figsize=(15,9))
